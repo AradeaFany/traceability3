249988a319956f59dff9300f2c6c7c8f83aabce3
Aradea Fany <aradeafany8@gmail.com>
2026-05-22 17:07:31 +0700

    add auth session dashboard rbac -Aradea

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -5,9 +5,57 @@ namespace App\Controllers;
 class Dashboard
 extends BaseController
 {
-
 public function index()
 {
+
+if(
+!session()->get('login')
+){
+
+return redirect()
+->to('/login');
+
+}
+
+
+$role =
+session()->get(
+'role'
+);
+
+
+if(
+$role=='supplier'
+){
+
+return view(
+'dashboard/supplier'
+);
+
+}
+
+
+if(
+$role=='pabrik'
+){
+
+return view(
+'dashboard/pabrik'
+);
+
+}
+
+
+if(
+$role=='distributor'
+){
+
+return view(
+'dashboard/distributor'
+);
+
+}
+
 
 return view(
 'dashboard/index'
